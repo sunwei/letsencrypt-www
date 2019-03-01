1005011a1fbdeeb7a07358a28d1ecef20bd9b706
sunwei <me@sunwei.xyz>
2019-03-01 20:46:31 +0800

    Modified: add find and remove for txt record; Wayde

--- provider/dnspod.sh
+++ provider/dnspod.sh
@@ -1,37 +1,80 @@
 #!/bin/bash
 set -e
 
-source '../lib/json.sh'
+source 'lib/json.sh'
+source 'lib/formatter.sh'
+source 'lib/http.sh'
+source 'lib/domain.sh'
 
 DNSPod_ID=83717
 DNSPod_TOKEN=32ff6aa5112b7bdaf64f48763c4788c4
+_LOGIN_TOKEN="${DNSPod_ID},${DNSPod_TOKEN}"
+DNSPod_RECORD_ID=
+_DOMAIN=
+_SUB_DOMAIN=
 
 _get_login_token() {
-  echo "${DNSPod_ID}.${DNSPod_TOKEN}"
+  echo "${DNSPod_ID},${DNSPod_TOKEN}"
+}
+
+_get_res_code() {
+  echo "${1}" | get_json_dict_value status | get_json_string_value code
+}
+
+_init_domain() {
+  local fullDomain="${1}"
+
+  _DOMAIN="$( <<<"${fullDomain}" get_domain )"
+  if [[ "$(has_sub_domain "${fullDomain}")" = True ]]; then
+    _SUB_DOMAIN="$( get_sub_domain <<<"${fullDomain}" )"
+  fi
 }
 
 create_txt_record() {
-  local domain="${1}" challengeToken="${2}" recordValue="${3}"
-
-  local loginToken="$(_get_login_token)"
+  local fullDomain="${1}" challengeToken="${2}" recordValue="${3}"
   local uri="https://dnsapi.cn/Record.Create"
-  local data="login_token=${loginToken}&format=json&domain=${domain}&sub_domain=_acme-challenge.abcd&record_type=TXT&value=${TOKEN_VALUE}&record_line=默认"
+  _init_domain "${fullDomain}"
 
-  data='{"login_token": "'"${loginToken}"'", "record_type": "'"${recordType}"'", "value": "'"${TOKEN_VALUE}"'", "domain": "sunzhongmou.com", "sub_domain": "_acme-challenge.abcd", "format": "json", "record_line": "默认"}'
-  curl -X POST https://dnsapi.cn/Record.Create -d "login_token=83717,32ff6aa5112b7bdaf64f48763c4788c4&format=json&domain=sunzhongmou.com&sub_domain=_acme-challenge.abcd&record_type=TXT&value=${TOKEN_VALUE}&record_line=默认"
+  local data="login_token=${_LOGIN_TOKEN}&domain=${_DOMAIN}&sub_domain=_acme-challenge.${_SUB_DOMAIN:-}&value=${recordValue}&record_type=TXT&format=json&record_line=默认"
+  local res="$(http_post "${uri}" "${data}" "$(http_get_content_type)" | clean_json)"
 
+  if [[ "$(_get_res_code "${res}")" = 1 ]]; then
+    DNSPod_RECORD_ID="$(echo "${res}" | get_json_dict_value record | get_json_string_value id )"
+    echo "${DNSPod_RECORD_ID}"
+  fi
 }
 
-get_txt_record() {
-  :
+find_txt_record() {
+  local fullDomain="${1}" challengeToken="${2}" recordValue="${3}"
+  local uri="https://dnsapi.cn/Record.List"
+  _init_domain "${fullDomain}"
+
+  local data="login_token=${_LOGIN_TOKEN}&domain=${_DOMAIN}&sub_domain=_acme-challenge.${_SUB_DOMAIN:-}&record_type=TXT&format=json"
+  local res="$(http_post "${uri}" "${data}" "$(http_get_content_type)" | clean_json)"
+  if [[ "$(_get_res_code "${res}")" = 1 ]]; then
+    echo True
+  else
+    echo False
+  fi
 }
 
 rm_txt_record() {
-  :
+  local fullDomain="${1}" recordID="${2}"
+  local uri="https://dnsapi.cn/Record.Remove"
+  _init_domain "${fullDomain}"
+
+  local data="login_token=${_LOGIN_TOKEN}&domain=${_DOMAIN}&record_id=${recordID}&format=json"
+  local res="$(http_post "${uri}" "${data}" "$(http_get_content_type)" | clean_json)"
+
+  if [[ "$(_get_res_code "${res}")" = 1 ]]; then
+    echo True
+  else
+    echo False
+  fi
 }
 
 HANDLER="${1}"
-if [[ "${HANDLER}" =~ ^(create_txt_record|get_txt_record|rm_txt_record)$ ]]; then
+if [[ "${HANDLER}" =~ ^(create_txt_record|find_txt_record|rm_txt_record)$ ]]; then
   shift
   "$HANDLER" "$@"
 fi
