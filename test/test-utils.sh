52e8633ef2aec568fb6ff7846b00959dadfd687e
sunwei <me@sunwei.xyz>
2019-03-11 20:57:10 +0800

    Modified: add tests for utils; Wayde

--- test/test-utils.sh
+++ test/test-utils.sh
@@ -13,7 +13,6 @@ teardown() {
   echo "tear down for utils tests..."
 }
 
-
 @test 'Should create a new tmp file' {
   tmp_file="$(mk_tmp_file)"
   assert [ -e "${tmp_file}" ]
@@ -24,6 +23,17 @@ teardown() {
   run exit_err "error message"
   assert_output "ERROR: error message"
   assert_failure
+}
+
+@test 'Should print message start with right mark' {
+  run check_msg "the message"
+  assert_output "✔ the message"
+  assert_success
+}
+
+@test 'Should get timestamp' {
+  run get_timestamp
+  assert_success
 }
 
 
